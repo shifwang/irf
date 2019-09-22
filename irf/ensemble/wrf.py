aa80bb9926a979c3f06af91e7cd903e16c750f2f
Eric Xia <ericzxia@gmail.com>
2019-09-22 16:27:53 -0700

    added feature_weights to regressor in wrf.py and tree.py

--- irf/ensemble/wrf.py
+++ irf/ensemble/wrf.py
@@ -27,4 +27,34 @@ class wrf(RandomForestClassifier):
                 # new feature importance score
                 feature_importances = self.feature_importances_
         return self
+
+#Eric: Doesn't lfook like much is changed here, as it looks like no significant differences between regressor and classifier
+#       come back to later to make sure there really is no difference                
+class wrf(RandomForestRegressor):
+    def fit(self, X, y, sample_weight=None, feature_weight=None, K = 5):
+        for k in range(K):
+            if k == 0:
+                # Initially feature weights are None
+                feature_importances = feature_weight
                 
+                # fit the classifier
+                super(wrf, self).fit(X=X,
+                         y=y,
+                         feature_weight=feature_importances)
+                
+                # Update feature weights using the
+                # new feature importance score
+                feature_importances = self.feature_importances_
+
+            else:
+                # fit weighted RF
+                # fit the classifier
+                super(wrf, self).fit(
+                        X=X,
+                        y=y,
+                        feature_weight=feature_importances)
+                
+                # Update feature weights using the
+                # new feature importance score
+                feature_importances = self.feature_importances_
+        return self
