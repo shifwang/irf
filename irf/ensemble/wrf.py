67527e46355bdb32c554582f03c3f1ca3d19aedb
Ubuntu <ubuntu@ip-172-31-20-162.us-west-2.compute.internal>
2019-09-25 21:02:35 +0000

    modify wrf.py

--- irf/ensemble/wrf.py
+++ irf/ensemble/wrf.py
@@ -30,7 +30,8 @@ class wrf(RandomForestClassifier):
 
 #Eric: Doesn't lfook like much is changed here, as it looks like no significant differences between regressor and classifier
 #       come back to later to make sure there really is no difference                
-class wrf(RandomForestRegressor):
+from .forest import RandomForestRegressor # Hue: need to import the class regressor first
+class wrf_reg(RandomForestRegressor): # Hue: change the name so that it does not clash with the first one.
     def fit(self, X, y, sample_weight=None, feature_weight=None, K = 5):
         for k in range(K):
             if k == 0:
@@ -38,7 +39,7 @@ class wrf(RandomForestRegressor):
                 feature_importances = feature_weight
                 
                 # fit the classifier
-                super(wrf, self).fit(X=X,
+                super(wrf_reg, self).fit(X=X,
                          y=y,
                          feature_weight=feature_importances)
                 
@@ -48,8 +49,8 @@ class wrf(RandomForestRegressor):
 
             else:
                 # fit weighted RF
-                # fit the classifier
-                super(wrf, self).fit(
+                # fit the regressor
+                super(wrf_reg, self).fit(
                         X=X,
                         y=y,
                         feature_weight=feature_importances)
